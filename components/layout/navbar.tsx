09c618b7cf4be564bcc18cc6797fccc0e142e3bb
Peddro <168378429+peddrof@users.noreply.github.com>
2025-08-04 21:15:28 -0300

    Updated navbar.tsx

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -70,8 +70,29 @@ export function Navbar() {
         {/* Logo */}
         <Link href="/" className="flex items-center space-x-2 group">
           <div className="p-2 bg-primary rounded-md group-hover:scale-105 transition-transform">
-            <Heart className="h-6 w-6 text-white" />
-          </div>
+  <svg
+    viewBox="0 0 24 24"
+    className="h-6 w-6"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="url(#diagonalGradient)"
+  >
+    <defs>
+      <linearGradient id="diagonalGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+        <stop offset="0%" stopColor="green" />
+        <stop offset="33.33%" stopColor="green" />
+        <stop offset="33.33%" stopColor="red" />
+        <stop offset="66.66%" stopColor="red" />
+        <stop offset="66.66%" stopColor="yellow" />
+        <stop offset="100%" stopColor="yellow" />
+      </linearGradient>
+    </defs>
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 
+    4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 
+    14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 
+    3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+  </svg>
+</div>
+
           <span className="text-xl font-bold text-gray-900">Voluntaria<span className="text-primary">+</span></span>
         </Link>
 
